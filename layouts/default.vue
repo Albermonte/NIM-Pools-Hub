8781fc75d3be566d5972877f3721dad1ee2f1dcd
Albermonte <alberto.monterroso@hotmail.com>
2020-03-16 21:26:51 +0100

    Pools on main

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,7 +10,7 @@
         disable-resize-watcher
         disable-route-watcher
         mini-variant-width="65"
-        height=""
+        height
         style="margin-top: 30vh"
         class="py-5"
       >
@@ -20,22 +20,22 @@
             class="d-flex flex-column justify-center"
             :class="sidebarOpen ? '' : 'align-center'"
           >
-            <template v-for="item in items">
+            <template v-for="pool in poolList">
               <v-list-item
-                :key="item.text"
+                :key="pool.displayName"
                 link
                 style="max-height: 60px; margin: 10px 15px 8px !important;"
-                :href="item.href"
+                :href="`/${pool.name}`"
               >
                 <v-list-item-action>
                   <img
-                    :src="item.icon"
+                    :src="pool.icon"
                     alt="avatar"
                     style="width: 35px; height: 35px; margin-right: 5px; border-radius: 50px"
-                  >
+                  />
                 </v-list-item-action>
                 <v-list-item-content>
-                  <v-list-item-title>{{ item.text }}</v-list-item-title>
+                  <v-list-item-title>{{ pool.displayName }}</v-list-item-title>
                 </v-list-item-content>
               </v-list-item>
             </template>
@@ -54,9 +54,7 @@
       left
       @click="show = true"
     >
-      <v-icon class="ml-6">
-        mdi-chevron-right
-      </v-icon>
+      <v-icon class="ml-6">mdi-chevron-right</v-icon>
     </v-btn>
 
     <v-app-bar clipped-left app color="blue darken-3" dark>
@@ -86,17 +84,7 @@
         </v-row>
       </v-container>
     </v-content>
-    <v-btn
-      bottom
-      color="pink"
-      dark
-      fab
-      fixed
-      right
-      nuxt
-      href="/setupMiner"
-      @click="testData"
-    >
+    <v-btn bottom color="pink" dark fab fixed right nuxt href="/setupMiner" @click="testData">
       <v-icon>mdi-help</v-icon>
     </v-btn>
     <v-overlay opacity="1" :value="overlay" color="#260133">
@@ -107,90 +95,129 @@
 </template>
 
 <script>
-import Loading from '~/components/Loading/Loading'
+import Loading from "~/components/Loading/Loading";
 
 export default {
   components: {
     Loading
   },
   data: () => ({
-    address: '',
+    address: "",
     overlay: true,
     dialog: false,
     mini: true,
     show: null,
     sidebarOpen: false,
-    items: [
-      { icon: '/nimpool.png', href: '/nimpool', text: 'NimPool' },
-      { icon: '/urp.jpg', href: '/urp', text: 'URP Best' },
-      { icon: '/siriuspool.png', href: '/siriuspool', text: 'SiriusPool' }
+    poolList: [
+      {
+        icon: "/nimpool.png",
+        name: "nimpool",
+        displayName: "NimPool",
+        message: "Preferred",
+        status: "",
+        extras: [" Pool fee: 1%", "Non-profit"]
+      },
+      {
+        icon: "/icemining.png",
+        name: "icemining",
+        displayName: "Icemining",
+        message: "YIIMP based",
+        status: "",
+        extras: ["Pool fee: 1.25%", "Greatest Support"]
+      },
+      {
+        icon: "/siriuspool.png",
+        name: "siriuspool",
+        displayName: "SiriusPool",
+        message: "Low Hashrate",
+        status: "",
+        extras: ["Pool fee: 1%", "Greek Pool"]
+      },
+      {
+        icon: "/skypool.png",
+        name: "skypool",
+        displayName: "Skypool",
+        message: "China based",
+        status: "",
+        extras: ["Pool fee: ~1%", "Not using official protocol"]
+      },
+      {
+        icon: "/urp.png",
+        name: "urp",
+        displayName: "URP Best",
+        message: "Lowest Fees",
+        status: "",
+        extras: [" Pool fee: 0.5%"]
+      }
     ]
   }),
-  beforeMount () {
-    this.overlay = this.$route.name !== 'index'
+  beforeMount() {
+    this.overlay = this.$route.name !== "index";
   },
-  mounted () {
+  mounted() {
     this.address = this.$store.state.localStorage.address.replace(
       /(.{4})/g,
-      '$1 '
-    )
+      "$1 "
+    );
 
     const loading = setInterval(() => {
       if (
-        typeof this.$store.state.urp.confirmed_balance !== 'undefined' &&
+        typeof this.$store.state.urp.confirmed_balance !== "undefined" &&
         this.overlay
       ) {
-        clearInterval(loading)
+        clearInterval(loading);
         setTimeout(() => {
-          this.overlay = false
-        }, 1800)
+          this.overlay = false;
+        }, 1800);
       }
-    }, 1000)
+    }, 1000);
   },
-  updated () {
-    if (this.address === null) { return }
-    this.address = this.address.toUpperCase()
-    let value = this.address
-    value = value.replace(/ /g, '')
-    value = value.substr(4) + value.substr(0, 4)
+  updated() {
+    if (this.address === null) {
+      return;
+    }
+    this.address = this.address.toUpperCase();
+    let value = this.address;
+    value = value.replace(/ /g, "");
+    value = value.substr(4) + value.substr(0, 4);
 
     const num = value
-      .split('')
-      .map((c) => {
-        const code = c.toUpperCase().charCodeAt(0)
-        return code >= 48 && code <= 57 ? c : (code - 55).toString()
+      .split("")
+      .map(c => {
+        const code = c.toUpperCase().charCodeAt(0);
+        return code >= 48 && code <= 57 ? c : (code - 55).toString();
       })
-      .join('')
-    let tmp = ''
+      .join("");
+    let tmp = "";
 
     for (let i = 0; i < Math.ceil(num.length / 6); i++) {
-      tmp = (parseInt(tmp + num.substr(i * 6, 6)) % 97).toString()
+      tmp = (parseInt(tmp + num.substr(i * 6, 6)) % 97).toString();
     }
 
     if (parseInt(tmp) === 1) {
       this.$store.commit(
-        'localStorage/updateAddress',
-        this.address.replace(/ /g, '')
-      )
+        "localStorage/updateAddress",
+        this.address.replace(/ /g, "")
+      );
       this.address = this.$store.state.localStorage.address.replace(
         /(.{4})/g,
-        '$1 '
-      )
+        "$1 "
+      );
     }
   },
   methods: {
-    clearAddress () {
-      this.$store.commit('localStorage/updateAddress', '')
+    clearAddress() {
+      this.$store.commit("localStorage/updateAddress", "");
     },
-    testData () {
-      this.address = 'NQ47 TD6C UT1K X35M DLVC N7QN YK5N FXNY SP5N'
+    testData() {
+      this.address = "NQ47 TD6C UT1K X35M DLVC N7QN YK5N FXNY SP5N";
     },
-    mouseover () {
-      this.sidebarOpen = true
+    mouseover() {
+      this.sidebarOpen = true;
     },
-    mouseleave () {
-      this.sidebarOpen = false
+    mouseleave() {
+      this.sidebarOpen = false;
     }
   }
-}
+};
 </script>
