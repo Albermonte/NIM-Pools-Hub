59e2ad6460d29265bfdaad068f3d6a3b33d6ef59
Albermonte <alberto.monterroso@hotmail.com>
2020-03-21 15:56:56 +0100

    Error linux fixed

--- components/downloadHelper.js
+++ components/downloadHelper.js
@@ -32,7 +32,8 @@ mkdir nq-miner
 tar -xzvf nq-miner-linux-0.99.6.tar.gz -C nq-miner
 rm nq-miner-linux-0.99.6.tar.gz
 rm ./nq-miner/start_gpu.sh
-printf "clear \\n #!/bin/bash \\n echo \"------------------------START Miner----------------------\" \\n ./nq-miner/nq-miner -t ${gpu} -a \"${address}\" -n \"NQ-Miner\" -p ${poolURL} \\n echo \"------------------------END Miner----------------------\" \\n echo \"Something went wrong or you exited\"" > start_gpu.sh
+printf "clear \\n #!/bin/bash \\n echo \\"------------------------START Miner----------------------\\" \\n ./nq-miner/nq-miner -t ${gpu} -a \\"${address}\\" -n \\"NQ-Miner\\" -p ${poolURL} \\n echo \\"------------------------END Miner----------------------\\" \\n echo \\"Something went wrong or you exited\\"" > start_gpu.sh
+chmod +x start_gpu.sh
 clear && printf "\\n\\n\\n\\n ############################################################## \\n ######                                                  ###### \\n ######  Go to NimiqMiner folder and open start_gpu.sh   ###### \\n ######                                                  ###### \\n ############################################################## \\n\\n\\n\\n Thanks \\n\\n\\n"                                               ###### & echo ######  Go to NimiqMiner folder and open start_gpu.bat  ###### & echo ######                                                  ###### & echo ############################################################## & echo. & echo. & echo. & echo. & echo. & echo. & echo Thanks & echo. & echo. & echo. & PAUSE
 `
     var element = document.createElement('a');
