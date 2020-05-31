0e5925bb25d7d5b9dab094f9aeaa80c5983b3595
Albermonte <alberto.monterroso@hotmail.com>
2020-05-31 18:33:05 +0200

    Notifications API added

---
+++ plugins/notifications.js
@@ -0,0 +1,25 @@
+if (!("Notification" in window)) {
+    console.warn('No Notification API')
+}
+// Let's check whether notification permissions have already been granted
+else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification("Hi there!");
+}
+// Otherwise, we need to ask the user for permission
+else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+            const options = {
+                body: 'We will notify you when payouts are received',
+                icon: '/nimpoolshub.png'
+            }
+            try {
+                var notification = new Notification('NIM Pools Hub', options);
+            } catch (err) {
+                console.log('Notification API error: ' + err);
+            }
+        }
+    });
+}
