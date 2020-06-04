c64c8c1be4b0f996104334bfbfff7b5a3ed67a91
Albermonte <alberto.monterroso@hotmail.com>
2020-06-04 11:57:47 +0200

    Working on native notifications

--- plugins/notifications.js
+++ plugins/notifications.js
@@ -1,10 +1,10 @@
-if (!("Notification" in window)) {
+/* if (!("Notification" in window)) {
     console.warn('No Notification API')
 }
 // Let's check whether notification permissions have already been granted
 else if (Notification.permission === "granted") {
     // If it's okay let's create a notification
-    /* var notification = new Notification("Hi there!"); */
+    //var notification = new Notification("Hi there!");
 }
 // Otherwise, we need to ask the user for permission
 else if (Notification.permission !== 'denied') {
@@ -22,4 +22,4 @@ else if (Notification.permission !== 'denied') {
             }
         }
     });
-}
+} */
