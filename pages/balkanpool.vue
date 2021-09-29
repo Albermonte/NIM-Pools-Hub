5b44bacde8088d0b0ec589f4664ec48e4ba03769
Alberto Monterroso <14013679+Albermonte@users.noreply.github.com>
2021-09-29 15:58:15 +0000

    Fixed bug when address was undefined

--- pages/balkanpool.vue
+++ pages/balkanpool.vue
@@ -1,11 +1,19 @@
 <template>
-  <v-layout column justify-center align-center style="margin-left: 3vw;">
-    <v-scroll-y-transition leave-absolute>
-      <UserInfo v-if="showUserInfo" class="container container--fluid py-0 my-n2" />
-      <AddressInput v-else />
-    </v-scroll-y-transition>
-    <GeneralInfo class="container container--fluid py-0 my-n2 pb-0" />
-  </v-layout>
+	<v-layout
+		column
+		justify-center
+		align-center
+		style="margin-left: 3vw;"
+	>
+		<v-scroll-y-transition leave-absolute>
+			<UserInfo
+				v-if="showUserInfo"
+				class="container container--fluid py-0 my-n2"
+			/>
+			<AddressInput v-else />
+		</v-scroll-y-transition>
+		<GeneralInfo class="container container--fluid py-0 my-n2 pb-0" />
+	</v-layout>
 </template>
 
 <script>
@@ -16,27 +24,27 @@ import AddressInput from "~/components/CustomVuetify/AddressInput";
 import pageTransition from "~/mixins/page-transitions.js";
 
 export default {
-  mixins: [pageTransition()],
-  components: {
-    UserInfo,
-    GeneralInfo,
-    AddressInput
-  },
-  data: () => ({
-    showUserInfo: false
-  }),
-  mounted() {
-    // If there's an address stored show the user info
-    this.showUserInfo = this.$store.state.localStorage.address !== "";
+	mixins: [pageTransition()],
+	components: {
+		UserInfo,
+		GeneralInfo,
+		AddressInput,
+	},
+	data: () => ({
+		showUserInfo: false,
+	}),
+	mounted() {
+		// If there's an address stored show the user info
+		this.showUserInfo = !!this.$store.state.localStorage.address;
 
-    // Watch until an address is stored to get statistics
-    this.$store.watch(this.$store.getters["localStorage/getAddress"], () => {
-      console.log("Address changed");
-      this.$store.dispatch(`${this.$route.name}/UPDATE_USER_INFO`);
-      this.$store.dispatch("nimiq/UPDATE_NIMIQ_INFO");
-      this.showUserInfo = this.$store.state.localStorage.address !== ""; // Hide if no address
-    });
-  },
-  methods: {}
+		// Watch until an address is stored to get statistics
+		this.$store.watch(this.$store.getters["localStorage/getAddress"], () => {
+			console.log("Address changed");
+			this.$store.dispatch(`${this.$route.name}/UPDATE_USER_INFO`);
+			this.$store.dispatch("nimiq/UPDATE_NIMIQ_INFO");
+			this.showUserInfo = !!this.$store.state.localStorage.address; // Hide if no address
+		});
+	},
+	methods: {},
 };
 </script>
--- pages/blankpool.vue
+++ pages/blankpool.vue
@@ -1,11 +1,19 @@
 <template>
-  <v-layout column justify-center align-center style="margin-left: 3vw;">
-    <v-scroll-y-transition leave-absolute>
-      <UserInfo v-if="showUserInfo" class="container container--fluid py-0 my-n2" />
-      <AddressInput v-else />
-    </v-scroll-y-transition>
-    <GeneralInfo class="container container--fluid py-0 my-n2 pb-0" />
-  </v-layout>
+	<v-layout
+		column
+		justify-center
+		align-center
+		style="margin-left: 3vw;"
+	>
+		<v-scroll-y-transition leave-absolute>
+			<UserInfo
+				v-if="showUserInfo"
+				class="container container--fluid py-0 my-n2"
+			/>
+			<AddressInput v-else />
+		</v-scroll-y-transition>
+		<GeneralInfo class="container container--fluid py-0 my-n2 pb-0" />
+	</v-layout>
 </template>
 
 <script>
@@ -16,29 +24,29 @@ import AddressInput from "~/components/CustomVuetify/AddressInput";
 import pageTransition from "~/mixins/page-transitions.js";
 
 export default {
-  mixins: [pageTransition()],
-  components: {
-    UserInfo,
-    GeneralInfo,
-    AddressInput
-  },
-  data: () => ({
-    showUserInfo: false
-  }),
-  mounted() {
-    // If there's an address stored show the user info
-    this.showUserInfo =
-      this.$store.state.localStorage.address !== "" &&
-      typeof this.$store.state.localStorage.address !== "undefined";
+	mixins: [pageTransition()],
+	components: {
+		UserInfo,
+		GeneralInfo,
+		AddressInput,
+	},
+	data: () => ({
+		showUserInfo: false,
+	}),
+	mounted() {
+		// If there's an address stored show the user info
+		this.showUserInfo =
+			this.$store.state.localStorage.address !== "" &&
+			typeof this.$store.state.localStorage.address !== "undefined";
 
-    // Watch until an address is stored to get statistics
-    this.$store.watch(this.$store.getters["localStorage/getAddress"], () => {
-      console.log("Address changed");
-      this.$store.dispatch(`${this.$route.name}/UPDATE_USER_INFO`);
-      this.$store.dispatch("nimiq/UPDATE_NIMIQ_INFO");
-      this.showUserInfo = this.$store.state.localStorage.address !== ""; // Hide if no address
-    });
-  },
-  methods: {}
+		// Watch until an address is stored to get statistics
+		this.$store.watch(this.$store.getters["localStorage/getAddress"], () => {
+			console.log("Address changed");
+			this.$store.dispatch(`${this.$route.name}/UPDATE_USER_INFO`);
+			this.$store.dispatch("nimiq/UPDATE_NIMIQ_INFO");
+			this.showUserInfo = !!this.$store.state.localStorage.address; // Hide if no address
+		});
+	},
+	methods: {},
 };
 </script>
